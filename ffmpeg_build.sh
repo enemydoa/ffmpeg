fb60ab0f69e0d66269cec78c644b82423d422936
enemydoa <enemydo@gmail.com>
2017-01-18 17:51:45 +0200

    Update ffmpeg_build.sh

--- ffmpeg_build.sh
+++ ffmpeg_build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-udo apt-get update
+sudo apt-get update
 sudo apt-get -y install autoconf automake build-essential libass-dev libfreetype6-dev libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texinfo zlib1g-dev
 
 mkdir ~/ffmpeg_sources
