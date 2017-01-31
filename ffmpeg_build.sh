f8bd8f21ac699ceb3814a12790ef047354aba52f
enemydoa <enemydo@gmail.com>
2017-01-31 15:31:35 +0200

    Update ffmpeg_build.sh

--- ffmpeg_build.sh
+++ ffmpeg_build.sh
@@ -6,9 +6,9 @@ mkdir ~/ffmpeg_sources
 
 #Yasm
 cd ~/ffmpeg_sources
-git clone --depth 1 git://github.com/yasm/yasm.git
-cd yasm
-autoreconf -fiv
+wget http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
+tar xzvf yasm-1.3.0.tar.gz
+cd yasm-1.3.0
 ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
 make
 make install
@@ -16,25 +16,29 @@ make distclean
 
 #libx264
 cd ~/ffmpeg_sources
-git clone --depth 1 git://git.videolan.org/x264
-cd x264
-PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
-make
+wget http://download.videolan.org/pub/x264/snapshots/last_x264.tar.bz2
+tar xjvf last_x264.tar.bz2
+cd x264-snapshot*
+PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --disable-opencl
+PATH="$HOME/bin:$PATH" make
 make install
 make distclean
 
 #libx265
+sudo apt-get install cmake mercurial
 cd ~/ffmpeg_sources
 hg clone https://bitbucket.org/multicoreware/x265
 cd ~/ffmpeg_sources/x265/build/linux
-cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
+PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
 make
 make install
+make distclean
 
 #libfdk_aac
 cd ~/ffmpeg_sources
-git clone --depth 1 git://git.code.sf.net/p/opencore-amr/fdk-aac
-cd fdk-aac
+wget -O fdk-aac.tar.gz https://github.com/mstorsjo/fdk-aac/tarball/master
+tar xzvf fdk-aac.tar.gz
+cd mstorsjo-fdk-aac*
 autoreconf -fiv
 ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
 make
@@ -42,24 +46,25 @@ make install
 make distclean
 
 #libmp3lame
+sudo apt-get install nasm
 cd ~/ffmpeg_sources
-curl -L -O http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
+wget http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
 tar xzvf lame-3.99.5.tar.gz
 cd lame-3.99.5
-./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-shared --enable-nasm
+./configure --prefix="$HOME/ffmpeg_build" --enable-nasm --disable-shared
 make
 make install
 make distclean
 
 #libopus
 cd ~/ffmpeg_sources
-git clone http://git.opus-codec.org/opus.git
-cd opus
-autoreconf -fiv
+wget http://downloads.xiph.org/releases/opus/opus-1.1.3.tar.gz
+tar xzvf opus-1.1.3.tar.gz
+cd opus-1.1.3
 ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
 make
 make install
-make distclean
+make clean
 
 #libogg
 cd ~/ffmpeg_sources
@@ -83,16 +88,18 @@ make distclean
 
 #libvpx
 cd ~/ffmpeg_sources
-git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
-cd libvpx
-./configure --prefix="$HOME/ffmpeg_build" --disable-examples
-make
+wget http://storage.googleapis.com/downloads.webmproject.org/releases/webm/libvpx-1.6.0.tar.bz2
+tar xjvf libvpx-1.6.0.tar.bz2
+cd libvpx-1.6.0
+PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --disable-examples --disable-unit-tests
+PATH="$HOME/bin:$PATH" make
 make install
 make clean
 
 #FFmpeg
 cd ~/ffmpeg_sources
-git clone http://source.ffmpeg.org/git/ffmpeg.git
+wget http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
+tar xjvf ffmpeg-snapshot.tar.bz2
 cd ffmpeg
 PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="$HOME/bin" --pkg-config-flags="--static" --enable-gpl --enable-nonfree --enable-libfdk-aac --enable-libfreetype --enable-libmp3lame --enable-libopus --enable-libtheora --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libx265
 make
